ec9e220b537ae4de2658cbff44568e7a92d4a442
SBTHDR <subrata.halder13d@gmail.com>
2020-09-02 21:00:15 +0600

    create todo with validation

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
@@ -14,6 +15,16 @@ class TodoController extends Controller
     public function create()
     {
         return view('todos.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255'
+        ]);
+        
+        Todo::create($request->all());
+        return redirect()->back()->with('message', 'Todo Created successfully');
     }
 
     public function edit()
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/todos', 'TodoController@index');
 Route::get('/todos/create', 'TodoController@create');
+Route::post('/todos/create', 'TodoController@store');
 Route::get('/todos/edit', 'TodoController@edit');
 
 Route::get('/', function () {
